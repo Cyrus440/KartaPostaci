3317c6179b50ae07dc117db7183d0974f7b07593
Filip Sobolewski <62287972+Cyrus440@users.noreply.github.com>
2022-05-25 13:36:37 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace KartaPostaci
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayInformationLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CharacterName = new System.Windows.Forms.Label();
+            this.CurrentNameTextBox = new System.Windows.Forms.TextBox();
+            this.CurrentRaceTextBox = new System.Windows.Forms.TextBox();
+            this.CharacterRace = new System.Windows.Forms.Label();
+            this.CurrentProfessionTextBox = new System.Windows.Forms.TextBox();
+            this.CharacterProfession = new System.Windows.Forms.Label();
+            this.UpdateProfessionTextBox = new System.Windows.Forms.TextBox();
+            this.UpdateRaceTextBox = new System.Windows.Forms.TextBox();
+            this.UpdateNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.LastButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.AddSheetButton = new System.Windows.Forms.Button();
+            this.DeleteSheetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayInformationLabel
+            // 
+            this.displayInformationLabel.AutoSize = true;
+            this.displayInformationLabel.Location = new System.Drawing.Point(317, 9);
+            this.displayInformationLabel.Name = "displayInformationLabel";
+            this.displayInformationLabel.Size = new System.Drawing.Size(60, 15);
+            this.displayInformationLabel.TabIndex = 0;
+            this.displayInformationLabel.Text = "Karta 0 z 0";
+            this.displayInformationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 1;
+            // 
+            // CharacterName
+            // 
+            this.CharacterName.AutoSize = true;
+            this.CharacterName.Location = new System.Drawing.Point(27, 77);
+            this.CharacterName.Name = "CharacterName";
+            this.CharacterName.Size = new System.Drawing.Size(83, 15);
+            this.CharacterName.TabIndex = 2;
+            this.CharacterName.Text = "Nazwa Postaci";
+            // 
+            // CurrentNameTextBox
+            // 
+            this.CurrentNameTextBox.Location = new System.Drawing.Point(27, 95);
+            this.CurrentNameTextBox.Name = "CurrentNameTextBox";
+            this.CurrentNameTextBox.ReadOnly = true;
+            this.CurrentNameTextBox.Size = new System.Drawing.Size(179, 23);
+            this.CurrentNameTextBox.TabIndex = 3;
+            this.CurrentNameTextBox.TabStop = false;
+            // 
+            // CurrentRaceTextBox
+            // 
+            this.CurrentRaceTextBox.Location = new System.Drawing.Point(212, 95);
+            this.CurrentRaceTextBox.Name = "CurrentRaceTextBox";
+            this.CurrentRaceTextBox.ReadOnly = true;
+            this.CurrentRaceTextBox.Size = new System.Drawing.Size(179, 23);
+            this.CurrentRaceTextBox.TabIndex = 5;
+            this.CurrentRaceTextBox.TabStop = false;
+            // 
+            // CharacterRace
+            // 
+            this.CharacterRace.AutoSize = true;
+            this.CharacterRace.Location = new System.Drawing.Point(212, 77);
+            this.CharacterRace.Name = "CharacterRace";
+            this.CharacterRace.Size = new System.Drawing.Size(72, 15);
+            this.CharacterRace.TabIndex = 4;
+            this.CharacterRace.Text = "Rasa Postaci";
+            this.CharacterRace.Click += new System.EventHandler(this.CharacterRace_Click);
+            // 
+            // CurrentProfessionTextBox
+            // 
+            this.CurrentProfessionTextBox.Location = new System.Drawing.Point(397, 95);
+            this.CurrentProfessionTextBox.Name = "CurrentProfessionTextBox";
+            this.CurrentProfessionTextBox.ReadOnly = true;
+            this.CurrentProfessionTextBox.Size = new System.Drawing.Size(179, 23);
+            this.CurrentProfessionTextBox.TabIndex = 7;
+            this.CurrentProfessionTextBox.TabStop = false;
+            // 
+            // CharacterProfession
+            // 
+            this.CharacterProfession.AutoSize = true;
+            this.CharacterProfession.Location = new System.Drawing.Point(397, 77);
+            this.CharacterProfession.Name = "CharacterProfession";
+            this.CharacterProfession.Size = new System.Drawing.Size(90, 15);
+            this.CharacterProfession.TabIndex = 6;
+            this.CharacterProfession.Text = "Profesja Postaci";
+            // 
+            // UpdateProfessionTextBox
+            // 
+            this.UpdateProfessionTextBox.Location = new System.Drawing.Point(397, 146);
+            this.UpdateProfessionTextBox.Name = "UpdateProfessionTextBox";
+            this.UpdateProfessionTextBox.Size = new System.Drawing.Size(179, 23);
+            this.UpdateProfessionTextBox.TabIndex = 10;
+            // 
+            // UpdateRaceTextBox
+            // 
+            this.UpdateRaceTextBox.Location = new System.Drawing.Point(212, 146);
+            this.UpdateRaceTextBox.Name = "UpdateRaceTextBox";
+            this.UpdateRaceTextBox.Size = new System.Drawing.Size(179, 23);
+            this.UpdateRaceTextBox.TabIndex = 9;
+            // 
+            // UpdateNameTextBox
+            // 
+            this.UpdateNameTextBox.Location = new System.Drawing.Point(27, 146);
+            this.UpdateNameTextBox.Name = "UpdateNameTextBox";
+            this.UpdateNameTextBox.Size = new System.Drawing.Size(179, 23);
+            this.UpdateNameTextBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Wpisz poniżej aby zaaktualizować";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Wpisz poniżej aby zaaktualizować";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Wpisz poniżej aby zaaktualizować";
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(27, 175);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(106, 23);
+            this.UpdateButton.TabIndex = 14;
+            this.UpdateButton.Text = "Zaktualizuj Kartę";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            // 
+            // LastButton
+            // 
+            this.LastButton.Location = new System.Drawing.Point(12, 386);
+            this.LastButton.Name = "LastButton";
+            this.LastButton.Size = new System.Drawing.Size(109, 23);
+            this.LastButton.TabIndex = 15;
+            this.LastButton.Text = "Poprzednia Karta";
+            this.LastButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(683, 386);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(105, 23);
+            this.NextButton.TabIndex = 16;
+            this.NextButton.Text = "Następna Karta";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // AddSheetButton
+            // 
+            this.AddSheetButton.Location = new System.Drawing.Point(12, 415);
+            this.AddSheetButton.Name = "AddSheetButton";
+            this.AddSheetButton.Size = new System.Drawing.Size(98, 23);
+            this.AddSheetButton.TabIndex = 17;
+            this.AddSheetButton.Text = "Dodaj Kartę";
+            this.AddSheetButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteSheetButton
+            // 
+            this.DeleteSheetButton.Location = new System.Drawing.Point(672, 415);
+            this.DeleteSheetButton.Name = "DeleteSheetButton";
+            this.DeleteSheetButton.Size = new System.Drawing.Size(116, 23);
+            this.DeleteSheetButton.TabIndex = 18;
+            this.DeleteSheetButton.Text = "Usuń Obecną Kartę";
+            this.DeleteSheetButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteSheetButton);
+            this.Controls.Add(this.AddSheetButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.LastButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UpdateProfessionTextBox);
+            this.Controls.Add(this.UpdateRaceTextBox);
+            this.Controls.Add(this.UpdateNameTextBox);
+            this.Controls.Add(this.CurrentProfessionTextBox);
+            this.Controls.Add(this.CharacterProfession);
+            this.Controls.Add(this.CurrentRaceTextBox);
+            this.Controls.Add(this.CharacterRace);
+            this.Controls.Add(this.CurrentNameTextBox);
+            this.Controls.Add(this.CharacterName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.displayInformationLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label displayInformationLabel;
+        private Label label2;
+        private Label CharacterName;
+        private TextBox CurrentNameTextBox;
+        private TextBox CurrentRaceTextBox;
+        private Label CharacterRace;
+        private TextBox CurrentProfessionTextBox;
+        private Label CharacterProfession;
+        private TextBox UpdateProfessionTextBox;
+        private TextBox UpdateRaceTextBox;
+        private TextBox UpdateNameTextBox;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Button UpdateButton;
+        private Button LastButton;
+        private Button NextButton;
+        private Button AddSheetButton;
+        private Button DeleteSheetButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace KartaPostaci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CharacterRace_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
